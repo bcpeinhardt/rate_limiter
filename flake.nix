43e2ebd4378946f786d236abc5ec15f5617b43e4
Benjamin Peinhardt <benjaminpeinhardt@gmail.com>
2025-07-29 14:34:40 -0500

    init flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "rate_limiter";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.gleam
+            pkgs.erlang
+            pkgs.rebar3
+          ];
+          shellHook = ''
+            echo "rate_limiter develop shell entered."
+          '';
+        };
+      });
+}
